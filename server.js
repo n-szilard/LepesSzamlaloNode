b091d6ad505dc793b8ffbb0d6296c6b0d603ea82
n-szilard <nagyapatiszilard@turr.hu>
2025-09-15 12:28:32 +0200

    Step endpointok

--- server.js
+++ server.js
@@ -12,8 +12,12 @@ app.use(express.json()); // json formatum megkövetelése
 app.use(express.urlencoded({extended: true})); // req body-n keresztül átmenjenek az adatok
 
 let users = [];
+let steps = [];
 const USERS_FILE = path.join(__dirname, 'users.json');
+const STEPS_FILE = path.join(__dirname, 'steps.json');
+
 loadUsers();
+loadSteps();
 
 // ENDPOINTS
 
@@ -21,6 +25,7 @@ app.get('/', (req, res) => {
     res.send('Türr Pista - 13.a szoftverfejlesztő lépegetés számláló backend api')
 });
 
+// ------------------- USERS --------------------
 
 // GET all users
 app.get("/users", (req, res) => {
@@ -126,6 +131,89 @@ app.post('/users/login', (req, res) => {
     res.send(loggedUser);
 });
 
+
+// -------------------- STEPS ---------------
+
+// GET all steps by userId
+app.get('/steps/user/:uid', (req, res) => {
+    let uid = Number(req.params.uid);
+    let matchSteps = [];
+    steps.forEach(step => {
+        if (step.uid === uid) {
+            matchSteps.push(step);
+        }
+    });
+    res.send(matchSteps);
+});
+
+
+// GET one step by id
+
+app.get('/steps/:id', (req, res) => {
+    let id = req.params.id;
+    let idx = steps.findIndex(step => step.id == id);
+    if (idx > -1) {
+        return res.send(steps[idx]);
+    }
+    return res.status(400).send({msg: "Nincs ilyen azonosítójú lépésszám!"});
+});
+
+// POST new step by uid
+
+app.post('/steps/upload/:uid', (req, res) => {
+    let data = req.body;
+    let uid = Number(req.params.uid);
+    steps.push(data);
+    data.id = getNextStepID();
+    data.uid = uid;
+    saveSteps();
+    res.send({msg: 'A lépés felvéve!'});
+})
+
+// PATCH step by id
+app.patch('/steps/:id', (req, res) => {
+    let data = req.body;
+    let id = Number(req.params.id);
+
+    let newDate = data.newDate;
+    let newCount = Number(data.newCount);
+
+    steps.forEach(step => {
+        if (step.id === id) {
+            step.date = newDate;
+            step.count = newCount;
+        }
+    });
+    saveSteps();
+    res.send({msg: 'Sikeres módosítás'})
+});
+
+
+// DELETE step by id
+app.delete('/steps/:id', (req, res) => {
+    let id = Number(req.params.id);
+    let idx = steps.findIndex(step => step.id == id);
+
+    if (idx > -1) {
+        steps.splice(idx, 1);
+        saveSteps();
+        return res.status(200).send({msg: 'Sikeres törlés'})
+    }
+    return res.status(400).send({msg: "Nincs ilyen azonosítójú lépésszám!"});
+})
+
+
+// DELETE all steps by userId
+app.delete('/steps/users/:uid', (req, res) => {
+    let uid = Number(req.params.uid);
+
+    let newSteps = steps.filter(step => step.uid != uid);
+    steps = newSteps;
+
+    saveSteps();
+    res.send({msg: `Siker?`})
+});
+
 app.listen(3000);
 
 function getNextID() {
@@ -136,7 +224,31 @@ function getNextID() {
     return maxId + 1;
 }
 
+function getNextStepID() {
+    const maxId = steps.reduce((max, u) => {
+        const id = Number(u?.id);
+        return Number.isFinite(id) && id > max ? id : max;
+    }, 0);
+    return maxId + 1;
+}
 
+function loadSteps(){
+    if (fs.existsSync(STEPS_FILE)) {
+        const raw = fs.readFileSync(STEPS_FILE);
+        try {
+            steps = JSON.parse(raw);
+        } catch (err) {
+            console.log('Hiba', error)
+            steps = []
+        }
+    } else {
+        saveSteps();
+    }
+}
+
+function saveSteps() {
+    fs.writeFileSync(STEPS_FILE, JSON.stringify(steps));
+}
 
 function loadUsers() {
     if (fs.existsSync(USERS_FILE)) {
